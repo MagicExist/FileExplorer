0225d94490428556e0345d3f1f3b9b7094b562c5
MagicExist <johhanparra1@gmail.com>
2025-11-24 23:26:43 -0500

    feat: show home dir items when the file explore starts

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static
+from textual.widgets import Header, Footer, Static, ListView, ListItem, Label
 from textual.containers import Container,Vertical,Horizontal
+from pathlib import Path
+#Modules
+from modules.basic_operations import list_dirs,Item,ItemType
 
 class FileExplorerApp(App):
     CSS_PATH =  "./main.css"
@@ -15,8 +18,8 @@ class FileExplorerApp(App):
                 classes="sidebar"
             ),
             Container(
-                Static("content",classes="title"),
-                classes="content"
+                classes="content",
+                id="content"
             ),
             classes="main-container"
         )
@@ -27,6 +30,18 @@ class FileExplorerApp(App):
         self.title = "File Explorer"
         self.sub_title = "v0.1" #file explorer version
 
+        
+
+    def on_ready(self) -> None:
+        #List items from home
+        home_items = list_dirs(Path.home())                                         #get items from "home" dir
+        list_view = ListView(id="files_list")
+        content_area = self.query_one("#content")
+        content_area.mount(list_view)                                               #mount the list_view before append items dinamically
+        for item in home_items:
+            if item.type == ItemType.FILE:
+                list_view.append(ListItem(Label(item.name)))
+        list_view.focus()                                                           #User start focus in the list view items to nav with arrows
 
 if __name__ == "__main__":
     app = FileExplorerApp()
